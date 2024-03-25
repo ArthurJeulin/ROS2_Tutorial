47bc8b3b2b2b0d5cb5c8fe820b1bbfd0ab808cdc
Arthur Jeulin <arthur.jeulin@coastautonomous.com>
2024-03-25 20:33:28 +0100

    written complete C++ Node

--- src/my_cpp_pck/src/my_first_node.cpp
+++ src/my_cpp_pck/src/my_first_node.cpp
@@ -1,14 +1,30 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode: public rclcpp::Node
+{
+  public:
+    MyNode(): Node("cpp_test"),_counter(0)
+    {
+      // Print
+      RCLCPP_INFO(this->get_logger(),"Hello Cpp Node");
+      _timer = this->create_wall_timer(std::chrono::seconds(1),
+                          std::bind(&MyNode::timerCallback,this));
+    }
+  private :
+    void timerCallback(){
+      RCLCPP_INFO(this->get_logger(),"Hello %d",_counter);
+      ++_counter;
+    }
+    rclcpp::TimerBase::SharedPtr _timer;
+    int _counter;
+};
 
 int main(int argc, char **argv)
 {
   // Initialisation of ROS 2 Communication
   rclcpp::init(argc,argv);
   // Create the node
-  auto node = std::make_shared<rclcpp::Node>("cpp_test");
-  // Print
-  RCLCPP_INFO(node->get_logger(),"Hello Cpp Node");
+  auto node = std::make_shared<MyNode>();
   /**
    * Pause the program and will keep the node alive till
    * Ctrl+C 
